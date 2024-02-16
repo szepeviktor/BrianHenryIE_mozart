60c76b6c3049c6a41bdc6c03426d823335497477
Brian Henry <BrianHenryIE@gmail.com>
2024-02-15 20:46:07 -0800

    Add empty test class
    Newer PHPUnit was complaining

--- tests/Unit/FileEnumeratorTest.php
+++ tests/Unit/FileEnumeratorTest.php
@@ -1,11 +1,23 @@
 <?php
-namespace BrianHenryIE\Strauss\Tests\Unit;
 
 // Verify there are no // double slashes in paths.
-
 
 // exclude_from_classmap
 
 // exclude regex
 
 // paths outside project directory
+
+namespace BrianHenryIE\Strauss\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FileEnumeratorTest
+ * @package BrianHenryIE\Strauss\Tests\Unit
+ * @coversDefaultClass \BrianHenryIE\Strauss\FileEnumerator
+ */
+class FileEnumeratorTest extends TestCase
+{
+
+}
